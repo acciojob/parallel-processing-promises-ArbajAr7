fff392efbb85e16e2769849759f662627d042f08
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-06-12 12:45:29 +0545

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function loadImage(url) {
+	return new Promise((resolve, reject) => {
+		let img = new Image();
+		img.onload = function() {
+			resolve(img);
+		};
+		img.onerror = function() {
+			reject(new Error(`Failed to load image's URL: ${url}`));
+		};
+		img.src = url;
+	});
+}
+
+document.getElementById("download-images-button").addEventListener("click", function () {
+	let imagePromises = images.map(img =>
+		loadImage(img.url));
+	Promise.all(imagePromises)
+	.then(imgs => {
+		imgs.forEach(img => document.getElementById("output").appendChild(img));
+	})
+	.catch(err => console.error(err));
+})
